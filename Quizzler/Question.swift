f0b03400aa133d8413e70c95cf4ce42f2857c98a
Abuzar Manzoor <abuzarmanzoor@hotmail.com>
2018-08-17 11:22:31 +0500

    Initial Commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Questions.swift
+//  Quizzler
+//
+//  Created by Abuzar Manzoor on 25/08/2017.
+//
+
+import Foundation
+
+class Question{
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+    
+}
